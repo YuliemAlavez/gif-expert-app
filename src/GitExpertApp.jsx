da9ac2b96aec88ec5514beb03951c05d3d920296
Yuliem Alavez <yuliemalavez@outlook.com>
2023-11-23 15:34:58 -0600

    Adding list of categories

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () =>{
+    const [categories, setCategories] = useState(['One punch']);
+
+    console.log(categories);
+
     return (
         <>
             {/* Title */}
             <h1>GifExpertApp</h1>
             {/* Input */}
             {/* List Gifs */}
+            <ol>
+                {/* Gif Items */}
+                { categories.map( category =>{
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+                
 
         </>
     )
