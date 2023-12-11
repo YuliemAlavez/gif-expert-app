4c41c1cc7fc87df048fc5d306bf4cb5fab4c2678
Yuliem Alavez <yuliemalavez@outlook.com>
2023-12-11 17:17:33 -0600

    106 Adding test to AddCategory

--- src/__tests__/components/AddCategory.test.jsx
+++ src/__tests__/components/AddCategory.test.jsx
@@ -1,5 +1,14 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { AddCategory } from "../../components";
+
 describe ('Test <AddCategory />', () => {
     it('Should be change text in the box', () => {
-        
+        render( <AddCategory onNewCategory={ ()=>{} }/>)
+        const input = screen.getByRole('textbox'); // Input and we have only one
+
+        fireEvent.input( input, { target: { value: 'Saitama'}} )
+        expect( input.value).toBe('Saitama');
+        //screen.debug();
+
     });
 });
