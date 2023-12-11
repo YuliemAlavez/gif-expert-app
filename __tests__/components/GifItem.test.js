fbc13ed5fd1942fee92179f980faddd084954e4c
Yuliem Alavez <yuliemalavez@outlook.com>
2023-12-11 16:37:46 -0600

    104 Testing GitItem

--- __tests__/components/GifItem.test.js
+++ __tests__/components/GifItem.test.js
@@ -9,4 +9,23 @@ describe ('GifItem', () => {
         const { container } = render( <GifItem title={title} url={url}/> )
         expect( container ).toMatchSnapshot();
     });
+
+    it ('Show image with URL', () => {
+        render( <GifItem title={title} url={url}/> );
+        //screen.debug(); // To show content
+        
+        // Form 1 to evaluate
+        /* expect( screen.getByRole('img').src).toBe(url);
+        expect( screen.getByRole('img').alt).toBe(title); */
+
+        // Form 1 to evaluate
+        const { src, alt }= screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    it ('Show text in the component', () => {
+        render( <GifItem title={title} url={url}/> );
+        expect( screen.getByText(title)).toBeTruthy();
+    });
 });
